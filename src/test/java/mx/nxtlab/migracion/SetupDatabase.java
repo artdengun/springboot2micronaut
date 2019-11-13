3cc819ef84c4e99db75d17fe6e2d1c8f1486b816
nxtlab@nxtlab.mx <nxtlab@nxtlab.mx>
2019-11-13 14:48:48 -0600

    Primer paso

--- src/test/java/mx/nxtlab/migracion/SetupDatabase.java
+++ src/test/java/mx/nxtlab/migracion/SetupDatabase.java
@@ -1,4 +1,25 @@
 package mx.nxtlab.migracion;
 
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
+
+@ManagedBean
+@Lazy(false)
 public class SetupDatabase {
+
+    @Inject
+    private JdbcTemplate jdbc;
+
+    @PostConstruct
+    public void init() {
+        var populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("tables.sql"));
+        populator.addScript(new ClassPathResource("data.sql"));
+        populator.execute(jdbc.getDataSource());
+    }
 }
