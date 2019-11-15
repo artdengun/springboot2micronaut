55e8e4c4ac4b3b2b79f130537f80eac592171b5c
nxtlab@nxtlab.mx <nxtlab@nxtlab.mx>
2019-11-15 12:37:42 -0600

    Paso 5

--- src/main/java/mx/nxtlab/migracion/MigracionApplication.java
+++ src/main/java/mx/nxtlab/migracion/MigracionApplication.java
@@ -1,13 +1,29 @@
 package mx.nxtlab.migracion;
-import javax.inject.Singleton;
-import javax.sql.DataSource;
 
-import io.micronaut.context.annotation.Factory;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.context.annotation.Value;
 import io.micronaut.runtime.Micronaut;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-@Factory
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Controller
 public class MigracionApplication {
+
+    @Singleton
+    public DataSource dataSource(@Value("${datasource.url}") String url,
+                                 @Value("${datasource.username}") String username,
+                                 @Value("${datasource.password}") String password,
+                                 @Value("${datasource.maxtotal}") int maxTotal
+                                 ){
+        DataSource ds = new DataSource();
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setMaxTotal(maxTotal);
+        return ds;
+    }
 
     @Singleton
     public JdbcTemplate jdbcTemplate(DataSource ds){
