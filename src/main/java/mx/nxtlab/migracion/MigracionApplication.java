fc16843eaf8f6c29bfaa75c13c7f60828b6052ab
nxtlab@nxtlab.mx <nxtlab@nxtlab.mx>
2019-11-15 12:22:17 -0600

    Paso 4

--- src/main/java/mx/nxtlab/migracion/MigracionApplication.java
+++ src/main/java/mx/nxtlab/migracion/MigracionApplication.java
@@ -1,8 +1,21 @@
 package mx.nxtlab.migracion;
+import javax.inject.Singleton;
+import javax.sql.DataSource;
 
+import io.micronaut.context.annotation.Factory;
 import io.micronaut.runtime.Micronaut;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@Factory
 public class MigracionApplication {
+
+    @Singleton
+    public JdbcTemplate jdbcTemplate(DataSource ds){
+        JdbcTemplate jdbc = new JdbcTemplate(ds);
+        jdbc.setDatabaseProductName("PostgresSQL");
+        return jdbc;
+    }
+
     public static void main(String[] args) {
         Micronaut.run(MigracionApplication.class, args);
     }
